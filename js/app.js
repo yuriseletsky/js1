f2c78bbf5882e4ac38b47a4cf97a1f5a1eb18fa2
Юрій Селецький <yuriseletsky@gmail.com>
2021-10-15 00:07:21 +0300

    add shows  elements from json

--- js/app.js
+++ js/app.js
@@ -1,31 +1,38 @@
 const tableElement = document.querySelector(".table-notes-items")
 const tableNotesParrent = document.querySelector(".table-notes")
+const tableSummaryParrent = document.querySelector(".table-summary")
+const unArchiveButton = document.querySelector("#show-archive")
 
 function getCategoryIcon(category) {
     switch (category) {
         case 'Task':
             return '<i class="fas fa-shopping-cart"></i>'
-            break;
         case 'Random Thought':
-            return '<i class="fas fa-shopping-cart"></i>'
-            break;
+            return '<i class="fas fa-head-side-virus"></i>'
         case 'Idea':
-            return '<i class="fas fa-shopping-cart"></i>'
-            break;
+            return '<i class="fas fa-lightbulb"></i>'
         case 'Quote':
-            return '<i class="fas fa-shopping-cart"></i>'
-            break;
+            return '<i class="fas fa-quote-right"></i>'
         default:
             return '<i class="fas fa-shopping-cart"></i>'
-            break;
     }
+}
+
+function renderSummaryTable() {
+
+}
+
+function getSummaryInfo() {
+
 }
 
 function createNoteElement(data) {
     let element = document.createElement('div');
-    element.className = 'table-notes-item';
+    let icon = getCategoryIcon(data.category);
+    element.className = `table-notes-item ${data.active === false && 'archived'}`;
     element.innerHTML = `
     <span class="note-info">
+        ${icon}
         ${data.name}
     </span>
     <span class="note-info">
@@ -55,14 +62,57 @@ function createNoteElement(data) {
     return element;
 }
 
+function createSummaryElement(name, data) {
+    let element = document.createElement('div');
+    let icon = getCategoryIcon(name);
+    element.className = 'table-notes-item';
+    element.innerHTML = `
+        <span class="note-info">
+            ${icon}
+            ${name}
+        </span>
+        <span class="note-info">
+            ${data.active}
+        </span>
+        <span class="note-info">
+            ${data.archived}
+        </span>
+`
+    return element;
+}
+
 fetch('./js/data.json')
     .then(response => response.json())
     .then(data => {
         data.notes.forEach(element => {
             let noteElement = createNoteElement(element)
             tableNotesParrent.append(noteElement)
-            console.log('1log', noteElement);
         });
+        return data.notes
+    })
+    .then(notes => {
+        let summary = new Map()
+        notes.forEach(note => {
+            let active = note.active ? 1 : 0;
+            let archived = note.active ? 0 : 1;
+            if (summary.has(note.category)) {
+                let current = summary.get(note.category)
+                summary.set(note.category, {
+                    'active': current.active + active,
+                    'archived': current.archived + archived
+                })
+            } else {
+                summary.set(note.category, {
+                    'active': active,
+                    'archived': archived
+                })
+            }
+        });
+        summary.forEach((value, noteName) => {
+            let summaryElement = createSummaryElement(noteName, value)
+            tableSummaryParrent.append(summaryElement)
+        });
+        // console.log(summary);
     })
     .catch((error) => {
         console.error('Error:', error);
